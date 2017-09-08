91fadd68af5fa1e380f908cd2c8fda606c943dff
Tyler Hevia <tylerhevia@gmail.com>
2017-09-08 15:41:03 -0600

    created users table, added seed data

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 "use strict";
 exports.__esModule = true;
-var express = require("express");
-var http = require("http");
-var app = express();
+const express = require("express");
+const http = require("http");
+const app = express();
+const bodyParser = require("body-parser");
+
 app.set("port", process.env.PORT || 3000);
 app.get("/", function(req, res) {
   res.send("test!");
@@ -12,10 +14,6 @@ app.set("port", process.env.PORT || 3000);
 app.get("/whatup", function(req, res) {
   res.send("whatup");
 });
-
-// app.set("port", process.env.PORT || 3000);
-// app.get("/math", function(req, res) {
-//   res.send(Math.random());
 
 http.createServer(app).listen(app.get("port"), function() {
   console.log("Express server listening on port " + app.get("port"));
